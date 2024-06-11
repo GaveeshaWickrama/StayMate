bc7b65e81502accf017546b37f06f2993e845aa7
Sachin <sachinelvitigala541@gmail.com>
2024-06-12 01:08:19 +0530

    Express-Auth-Update: Added routes for register and verify otp

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser } = require('../controllers/authController');
+const { loginUser, requestRegister, verifyOtp } = require('../controllers/authController');
 
 router.post('/login', loginUser);
-
+router.post('/register', requestRegister);
+router.post('/verify-otp', verifyOtp);
 
 module.exports = router;
+
 
