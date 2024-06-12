19a8fb14c9b7aa8416273b2badd20cb36ed20f14
Sachin <sachinelvitigala541@gmail.com>
2024-06-12 18:00:18 +0530

    Express-User: Tests for user login

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,43 +1,36 @@
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
 
-const userController = {
-  createUser: async (req, res) => {
+async function getUser(req, res) {
     try {
-      const { email, password, role } = req.body;
-
-      // Check if the user already exists
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'User already exists' });
-      }
-
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // Create a new user
-      const newUser = new User({
-        email,
-        password: hashedPassword,
-        role
-      });
-
-      // Save the user to the database
-      const savedUser = await newUser.save();
-
-      res.status(201).json({
-        message: 'User created successfully',
-        user: {
-          id: savedUser._id,
-          email: savedUser.email,
-          role: savedUser.role,
-          createdOn: savedUser.createdOn
-        }
-      });
-    } catch (error) {
-      res.status(500).json({ message: 'Server error', error });
+        const userId = req.user.userId;
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: 'Not found' });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-  }
-};
+}
 
-module.exports = userController;
+async function updateUser(req, res) {
+    try {
+        const userId = req.user.userId;
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: 'Not found' });
+
+        Object.keys(req.body).forEach(key => {
+            user[key] = req.body[key];
+        });
+
+        const updatedUser = await user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+module.exports = {
+  
+    getUser,
+    updateUser,
+   
+};
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { authToken } = require('../middleware/authProvider')
 
-router.post('/register', userController.createUser);
+// get self user info (id stored in jwt token)
+router.get('/', authToken, userController.getUser);
+
+// patch self user info (id stored in jwt token)
+router.patch('/', authToken, userController.updateUser);
+
 
 module.exports = router;
