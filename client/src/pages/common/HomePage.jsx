57c6b120db14a593069c1d862fe8e6df190be9b2
Chamudi Siriwardhane <116910122+heittre@users.noreply.github.com>
2024-06-17 17:49:24 +0530

    Add tailwind

--- client/src/pages/common/HomePage.jsx
+++ client/src/pages/common/HomePage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function HomePage() {
-  return <h1>Welcome to the (PUBLIC) Home Page</h1>;
+  // return <h1>Welcome to the (PUBLIC) Home Page</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default HomePage;
