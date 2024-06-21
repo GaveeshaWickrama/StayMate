121fcbe77db0946029cb515842149560e4f45a1d
Sachin <sachinelvitigala541@gmail.com>
2024-06-21 19:24:18 +0530

    added some temp pages for host

---
+++ client/src/pages/host/HostListings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HostListings() {
+  return (
+    <div>
+      <h1>Your Listings</h1>
+      <p>Manage your property listings here.</p>
+    </div>
+  );
+}
+
+export default HostListings;
