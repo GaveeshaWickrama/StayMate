3ac8251ff723f202985ee937c34eacb3a691ae8f
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-06-23 17:54:51 +0530

    create model for reservations

---
+++ server/src/models/reservationModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  property: {
+    type: Schema.Types.ObjectId,
+    ref: "property",
+    required: true,
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+  checkOutDate: {
+    type: Date,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  noOfGuests: {
+    type: Number,
+    required: true,
+  },
+  paymentStatus: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Reservation", reservationSchema);
