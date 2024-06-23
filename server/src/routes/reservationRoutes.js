fac187378db2fc19071eda2c55c99fecb0fed73d
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-06-23 17:55:06 +0530

    add routes for save reservation

---
+++ server/src/routes/reservationRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const reservationController = require("../controllers/reservationController");
+const { authToken, requireRole } = require("../middleware/authProvider"); // Adjust as necessary
+
+// book a property
+router.post(
+  "/add",
+  authToken,
+  requireRole("guest"),
+  reservationController.addReservation
+);
+
+module.exports = router;
