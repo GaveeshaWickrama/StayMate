29e075f361b2ed98ba1e6f4753d122efeb62bcee
Chamudi Siriwardhane <116910122+heittre@users.noreply.github.com>
2024-06-27 20:44:18 +0530

    finalized uis for technician as of now

--- client/src/routes/TechnicianRoutes.jsx
+++ client/src/routes/TechnicianRoutes.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from '../context/auth';
-import TechnicianRequests from '../pages/technician/MyProfile';
 
-import AdminPage from '../pages/technician/AdminPage';
-import EditProfile from '../pages/technician/EditProfile';
-import HostListings from '../pages/technician/HostListings';
-import HostPage from '../pages/technician/HostPage';
-import Reservations from '../pages/technician/Reservations';
-import MyProfile from '../pages/technician/MyProfile';
-import RaiseComplaint from '../pages/technician/RaiseComplaintPage';
 import PendingTasks from '../pages/technician/PendingTasks';
 import ActiveTasks from '../pages/technician/ActiveTasks';
 import UploadProof from '../pages/technician/Completion';
@@ -19,17 +11,10 @@ function UserRoutes() {
 
   return (
     <Routes>
-      <Route path="/another-profile" element={<TechnicianRequests/>} />
       <Route path="/requests/pending-tasks" element={<PendingTasks/>} />
       <Route path="/requests/active-tasks" element={<ActiveTasks/>} />
       <Route path="/requests/completion" element={<UploadProof/>} />
-      <Route path="/admin-profile" element={<AdminPage/>} />
-      <Route path="/edit-profile" element={<EditProfile/>} />
-      <Route path="/listings" element={<HostListings/>} />
-      <Route path="/hostpage" element={<HostPage/>} />
-      <Route path="/my-profile" element={<MyProfile/>} />
-      <Route path="/raise-complaint" element={<RaiseComplaint/>} />
-      <Route path="/reservations" element={<Reservations/>} />
+     
     </Routes>
   );
 }
