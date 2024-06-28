b9ee3a6e9f5593375e1ec4ac5a57514f800888d7
Sanandi Sithumya <2021is092@stu.ucsc.cmb.ac.lk>
2024-06-29 00:25:22 +0530

    added admin dashboard

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from '../context/auth';
-import AdminPage from '../pages/admin/AdminPage';
+import AdminDashboard from '../pages/admin/AdminDashboard';
 import MyProfile from '../pages/admin/MyProfile';
 import EditProfile from '../pages/admin/EditProfile';
+import UserCenter from '../pages/admin/UserCenter';
 
 function AdminRoutes() {
   const { currentUser, loading } = useAuth();
@@ -14,9 +15,10 @@ function AdminRoutes() {
 
   return (
     <Routes>
-      <Route path="/" element={currentUser && currentUser.role === 'admin' ? <AdminPage /> : <Navigate to="/Unauthorized" />} />
+      <Route path="/" element={currentUser && currentUser.role === 'admin' ? <AdminDashboard /> : <Navigate to="/Unauthorized" />} />
       <Route path="/myprofile" element={currentUser && currentUser.role === 'admin' ? <MyProfile /> : <Navigate to="/Unauthorized" />} />
       <Route path="/editprofile" element={currentUser && currentUser.role === 'admin' ? <EditProfile /> : <Navigate to="/Unauthorized" />} />
+      <Route path="/UserCenter" element={currentUser && currentUser.role === 'admin' ? <UserCenter /> : <Navigate to="/Unauthorized" />} />
     </Routes>
   );
 }
