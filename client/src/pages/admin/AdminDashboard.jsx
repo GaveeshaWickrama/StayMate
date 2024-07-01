a51007970b73de6db57f6b7d92bf426219974e0b
Sanandi Sithumya <2021is092@stu.ucsc.cmb.ac.lk>
2024-07-01 11:48:16 +0530

    trial

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -10,7 +10,7 @@ function AdminDashboard() {
     { name: 'April', uv: 2780, pv: 3908, amt: 2000 },
     { name: 'May', uv: 1890, pv: 4800, amt: 2181 },
     { name: 'June', uv: 2390, pv: 3800, amt: 2500 },
-    { name: 'July', uv: 3490, pv: 4300, amt: 2100 },
+    { name: 'July', uv: 3490, pv: 4300, amt: 2100 },kkkkkkkkkkkkkkkkkkkkkk
   ];
 
   return (
