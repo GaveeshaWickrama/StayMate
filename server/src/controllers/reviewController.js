2f1b9b69257fe60d8663b93d006ea774f6fa17d8
Sanandi Sithumya <2021is092@stu.ucsc.cmb.ac.lk>
2024-07-04 13:45:13 +0530

    reviews
    reviews

--- server/src/controllers/reviewController.js
+++ server/src/controllers/reviewController.js
@@ -39,6 +39,22 @@ const addReview = async (req, res) => {
   }
 };
 
+// Fetch reviews related to properties owned by the host
+ const getHostReviews = async (req, res) => {
+   const hostId = req.user.userId; // Assuming host ID is available in req.user
+
+   try {
+     const reviews = await Review.find({ "property.host": hostId })
+       .populate("user", "username") // Populate the 'user' field with 'username'
+       .populate("reservation", "booking_date") // Populate the 'reservation' field with 'booking_date'
+       .populate("property", "name"); // Populate the 'property' field with 'name'
+
+     res.status(200).json(reviews);
+   } catch (error) {
+     console.error("Error fetching host reviews:", error);
+     res.status(500).json({ message: "Server error" });
+   }
+ };
 module.exports = {
-  addReview,
+  addReview, getHostReviews
 };
--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const reviewController = require("../controllers/reviewController");
 const { authToken, requireRole } = require("../middleware/authProvider"); // Adjust as necessary
 
-// add review
-router.post( "/add", authToken, requireRole("guest"), reviewController.addReview );
+// Add review
+router.post("/add", authToken, requireRole("guest"), reviewController.addReview);
+
+// Get host reviews
+router.get("/hostreviews", authToken, requireRole("host"), reviewController.getHostReviews);
 
 module.exports = router;
