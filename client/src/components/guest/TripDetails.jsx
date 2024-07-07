3ae4aeaea6486169afc999b617d0868f369b130f
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-07-07 20:08:46 +0530

    Make code Responsive

--- client/src/components/guest/TripDetails.jsx
+++ client/src/components/guest/TripDetails.jsx
@@ -9,56 +9,50 @@ const TripDetails = ({ trip, isUpcoming, isOngoing, isCompleted }) => {
       : img1; // Use local image as fallback
 
   return (
-    <div className="p-4 border rounded-lg shadow-md mb-4 w-[900px] mt-[50px] ml-[10px] flex">
-      <div className="w-1/3">
+    <div className="p-4 border rounded-lg shadow-md mb-4 mt-12 mx-2 md:mx-auto md:w-3/4 flex flex-col md:flex-row ">
+      <div className="md:w-1/3 mb-4 md:mb-0">
         <img
           src={imageUrl}
           alt="Property"
-          className="w-5/6 h-48 object-cover rounded-l-lg"
+          className="w-full h-48 object-cover rounded-l-lg"
         />
       </div>
-      <div className="w-2/3 p-4 flex flex-col justify-between">
+      <div className="md:w-2/3 p-4 flex flex-col justify-between">
         <div>
-          <h3 className="text-md font-semibold ml-[30px]">
-            {trip.property.title}
-          </h3>
-          <p className="text-sm text-gray-600 ml-[30px]">
-            {trip.property.location.city}
-          </p>
-          <p className="text-sm text-gray-600 ml-[30px]">
+          <h3 className="text-md font-semibold">{trip.property.title}</h3>
+          <p className="text-sm text-gray-600">{trip.property.location.city}</p>
+          <p className="text-sm text-gray-600">
             {new Date(trip.checkInDate).toLocaleDateString()} -{" "}
             {new Date(trip.checkOutDate).toLocaleDateString()}
           </p>
-          <p className="text-sm text-gray-600 ml-[30px]">
-            {trip.noOfGuests} Guests
+          <p className="text-sm text-gray-600">
+            {trip.noOfGuests} {trip.noOfGuests === 1 ? "Guest" : "Guests"}
           </p>
-          <p className="text-md font-semibold mt-2 ml-[30px]">
-            RS {trip.totalPrice}
-          </p>
+          <p className="text-md font-semibold mt-2">RS {trip.totalPrice}</p>
         </div>
-        <div className="mt-4 flex justify-between items-center">
+        <div className="mt-4 flex flex-col md:flex-row justify-between items-center">
           {isUpcoming ? (
-            <button className="font-semibold text-white text-sm px-4 py-2 bg-red-500 border border-red-500 rounded ml-[150px] mt-[50px]">
+            <button className="font-semibold text-white text-sm px-4 py-2 bg-red-500 border border-red-500 rounded mt-2 md:mt-0">
               Cancel Reservation
             </button>
           ) : isOngoing ? (
             <>
               <button
                 onClick={() => (window.location.href = "#")}
-                className="font-semibold text-white text-sm px-10 py-2 bg-blue-500 border border-blue-500 rounded ml-[150px] mt-[50px]"
+                className="font-semibold text-white text-sm px-10 py-2 bg-blue-500 border border-blue-500 rounded mt-2 md:mt-0 md:ml-2"
               >
                 Chat
               </button>
               <button
                 onClick={() => (window.location.href = "#")}
-                className="font-semibold text-white text-sm px-6 py-2 bg-yellow-500 border border-yellow-500 rounded mt-[50px]"
+                className="font-semibold text-white text-sm px-6 py-2 bg-yellow-500 border border-yellow-500 rounded mt-2 md:mt-0 md:ml-2"
               >
                 Add Complaint
               </button>
             </>
           ) : isCompleted ? (
             <>
-              <span className="font-semibold text-white text-sm px-4 py-2 bg-green-500 border border-green-500 rounded ml-[150px] mt-[50px]">
+              <span className="font-semibold text-white text-sm px-4 py-2 bg-green-500 border border-green-500 rounded mt-2 md:mt-0">
                 Completed
               </span>
               <button
@@ -66,7 +60,7 @@ const TripDetails = ({ trip, isUpcoming, isOngoing, isCompleted }) => {
                   (window.location.href =
                     "http://localhost:5173/user/reviews/add")
                 }
-                className="font-semibold text-white text-sm px-4 py-2 bg-blue-500 border border-blue-500 rounded mt-[50px]"
+                className="font-semibold text-white text-sm px-4 py-2 bg-blue-500 border border-blue-500 rounded mt-2 md:mt-0 md:ml-2"
               >
                 Add Review
               </button>
