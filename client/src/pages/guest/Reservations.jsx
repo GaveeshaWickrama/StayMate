9a8919c909b00fb243402fb2b9f395e391b32066
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-07-07 20:09:53 +0530

    make Reservation Page Responsive

--- client/src/pages/guest/Reservations.jsx
+++ client/src/pages/guest/Reservations.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import TripDetails from "../../components/guest/TripDetails";
 import TabButtons from "../../components/guest/TabButtons";
-import { useReservation } from "../../context/ReservationContext";
+import { useStore } from "../../context/StoreContext";
 
 const Reservation = () => {
-  const { userReservations } = useReservation();
+  const { userReservations } = useStore();
   const [activeTab, setActiveTab] = useState("upcoming");
 
   // Function to compare two trips by booking date (assuming trip.created_at)
@@ -16,27 +16,29 @@ const Reservation = () => {
   const sortedReservations = [...userReservations].sort(compareByBookingDate);
 
   return (
-    <div className="container mx-auto p-4 mt-[100px] ml-[100px]">
-      <TabButtons activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div>
-        {activeTab === "upcoming" &&
-          sortedReservations
-            .filter((trip) => trip.status === "upcoming")
-            .map((trip, index) => (
-              <TripDetails key={index} trip={trip} isUpcoming={true} />
-            ))}
-        {activeTab === "ongoing" &&
-          sortedReservations
-            .filter((trip) => trip.status === "ongoing")
-            .map((trip, index) => (
-              <TripDetails key={index} trip={trip} isOngoing={true} />
-            ))}
-        {activeTab === "completed" &&
-          sortedReservations
-            .filter((trip) => trip.status === "completed")
-            .map((trip, index) => (
-              <TripDetails key={index} trip={trip} isCompleted={true} />
-            ))}
+    <div className="container mx-auto p-4 mt-24 ml-4">
+      <div className="mx-4 ml-[50px]">
+        <TabButtons activeTab={activeTab} setActiveTab={setActiveTab} />
+        <div className="mt-6">
+          {activeTab === "upcoming" &&
+            sortedReservations
+              .filter((trip) => trip.status === "upcoming")
+              .map((trip, index) => (
+                <TripDetails key={index} trip={trip} isUpcoming={true} />
+              ))}
+          {activeTab === "ongoing" &&
+            sortedReservations
+              .filter((trip) => trip.status === "ongoing")
+              .map((trip, index) => (
+                <TripDetails key={index} trip={trip} isOngoing={true} />
+              ))}
+          {activeTab === "completed" &&
+            sortedReservations
+              .filter((trip) => trip.status === "completed")
+              .map((trip, index) => (
+                <TripDetails key={index} trip={trip} isCompleted={true} />
+              ))}
+        </div>
       </div>
     </div>
   );
