ca4935a67b2895cb2ee57a934729adade7fce519
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-07-08 21:19:10 +0530

    Update propertyRoutes.js

--- server/src/routes/propertyRoutes.js
+++ server/src/routes/propertyRoutes.js
@@ -1,17 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const propertyController = require('../controllers/propertyController');
-const { authToken, requireRole } = require('../middleware/authProvider'); // Adjust as necessary
-const upload = require('../middleware/multer'); // Import multer middleware
+const propertyController = require("../controllers/propertyController");
+const { authToken, requireRole } = require("../middleware/authProvider"); // Adjust as necessary
+const upload = require("../middleware/multer"); // Import multer middleware
 
 // Create a new property (accessible by hosts and admins)
-router.post('/add', authToken, requireRole('host', 'admin'), upload.array('images', 10), propertyController.createProperty);
+router.post(
+  "/add",
+  authToken,
+  requireRole("host", "admin"),
+  upload.array("images", 10),
+  propertyController.createProperty
+);
 
-router.get('/host-properties', authToken, requireRole('host', 'admin'), propertyController.getPropertiesByHostId);
+router.get(
+  "/host-properties",
+  authToken,
+  requireRole("host", "admin"),
+  propertyController.getPropertiesByHostId
+);
 // router.get('/host-properties', authToken, requireRole('host', 'admin'), propertyController.getPropertiesByHostId);
 // Get property by ID
-router.get('/:id', propertyController.getPropertyById);
+router.get("/:id", propertyController.getPropertyById);
+
+// Get all properties (accessible by everyone)
+router.get("/", propertyController.getAllProperties);
 
 module.exports = router;
-
-
