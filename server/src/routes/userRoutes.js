0d64a578cb1cd92614e6497cd9356da65dda60e0
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-07-10 20:37:47 +0530

    backend configuration for all users to view and update profiles

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,10 +3,20 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { authToken, requireRole } = require('../middleware/authProvider');
 
-// get self user info (id stored in jwt token)
-router.get('/', authToken, requireRole('user', 'admin', 'guest', 'host', 'technician'), userController.getUser);
+//get all users
+router.get('/',userController.getUsers)
+
+//get a single user
+router.get('/:id',userController.getUser)
+
+//delete a user
+router.delete('/:id',userController.deleteUser)
+
+//update a user
+router.patch('/:id',userController.updateUser)
+
 
 // patch self user info (id stored in jwt token)
-router.patch('/', authToken, requireRole('user', 'admin', 'guest', 'host', 'technician'), userController.updateUser);
+router.patch('/:id', authToken, requireRole('user', 'admin', 'guest', 'host', 'technician'), userController.updateUser);
 
 module.exports = router;
