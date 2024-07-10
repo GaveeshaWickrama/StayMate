0ee7ca07619468a526d96f7967bf402f55a26b24
Sachin <sachinelvitigala541@gmail.com>
2024-07-10 18:29:03 +0530

    Test

--- client/src/pages/common/HomePage.jsx
+++ client/src/pages/common/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
       
-        Welcome to the (PUBLIC) Home Page 
+        Welcome to the (PUBLIC) Home Page !
       </h1>
     </div>
   );
