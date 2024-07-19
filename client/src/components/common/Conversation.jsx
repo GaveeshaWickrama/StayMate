8deaec3b745a0c8696a4c7ec745b9b9ac29d6f39
Charith Lakshan <wgclakshan@gmail.com>
2024-07-19 23:41:39 +0530

    Added logic to change the background color of selected conversation

--- client/src/components/common/Conversation.jsx
+++ client/src/components/common/Conversation.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation';
 
 const Conversation = ({conversation,lastIdx,emoji}) => {
 
+    const {selectedConversation, setSelectedConversation}= useConversation();
+
+    const isSelected = selectedConversation?._id === conversation._id 
+    console.log("isSelected",isSelected);
+    console.log("conversation",conversation);
+
     const imageUrl = conversation.picture
-    ? conversation.picture
+    ? conversation.picture  
     : 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp';
 
   return <>
-    <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+    <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+        ${isSelected ? "bg-sky-500" : "" }
+        `}
+        onClick={() => setSelectedConversation(conversation)}
+        >
         
         <div className='avatar online'>
             <div className='w-12 rounded-full'>
