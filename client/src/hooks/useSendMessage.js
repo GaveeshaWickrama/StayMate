a7338fd8a4154f0cd23cc45f0a9e9eaedc53246f
Charith Lakshan <wgclakshan@gmail.com>
2024-07-20 15:14:37 +0530

    send Message Functionality

---
+++ client/src/hooks/useSendMessage.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import useConversation from "../zustand/useConversation";
+import { toast } from "react-toastify";
+import { useAuth } from "../context/auth";
+import axios from "axios";
+
+const useSendMessage = () => {
+
+    const [loading,setLoading] = useState(false);
+    const {messages,setMessages,selectedConversation} = useConversation(); 
+    const { token } = useAuth()
+
+    const sendMessage = async (message) => {
+        setLoading(true);
+        try {
+            const res = await axios.post(
+                `${import.meta.env.VITE_API_URL}/message/send/${selectedConversation._id}`,
+                {message},
+                {
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                     'Content-Type': 'multipart/form-data'
+                  },
+                }
+            );
+            
+            const data = res.data()
+            if(data.error) throw new Error(data.error);
+
+            setMessages([...messages,data]);
+        } catch (error) {
+            console.log("send message error : ",error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { sendMessage, loading };
+}
+
+export default useSendMessage;
