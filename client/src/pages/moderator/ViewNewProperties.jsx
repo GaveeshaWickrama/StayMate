0a1b62adba338cdc13bcb0a2ea68c9402d807a1b
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-07-21 09:06:31 +0530

    Changed the UI for moderator viewing new property listed

--- client/src/pages/moderator/ViewNewProperties.jsx
+++ client/src/pages/moderator/ViewNewProperties.jsx
@@ -1,74 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-// import logo from '../../../../uploads/1720194545293-img1.jpg.webp';
-//  D:\Universtiy Academic\3rd Year - 1st Sem\SCS3214  - Group Project 2\Project\StayMate\server\
+import { useAuth } from '../../context/auth';
+
+// components
+import  PendingPropertyCard from '../../components/moderator/PendingPropertyCard'
+import  SearchProperty from '../../components/moderator/SearchProperty'
 
 const ViewNewProperties = () => {
+
+    const { token } = useAuth();
     const [properties, setProperties] = useState([]);
 
-    // useEffect(() => {
-    //     // Fetch properties from the PropertyVerifiedModel
-    //     const fetchProperties = async () => {
-    //         try {
-    //             const response = await axios.get('/api/properties/verified');
-    //             setProperties(response.data);
-    //         } catch (error) {
-    //             console.error('Error fetching properties:', error);
-    //         }
-    //     };
+    useEffect(() => {
+        
+        const fetchProperties = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/moderator/viewNewProperties`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (response.status === 200) {
+                    const json = response.data;
+                    setProperties(json);
+                } else {
+                        console.error('Failed to fetch properties. Status:', response.status);
+                }
 
-    //     fetchProperties();
-    // }, []);
+            } catch (error) {
+                console.error('Error fetching properties:', error);
+            }
+        };
+
+        fetchProperties();
+    }, [token]);
 
     return (
-        <div className="container mx-auto px-4 py-8">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                {/* {properties.map((property) => ( */}
-                    <div 
-                    // key={property._id} 
-                    className="bg-white rounded-lg shadow-lg overflow-hidden">
-                        <img 
-                            className="w-full h-48 object-cover"
-                            src= 'http://localhost:3000/uploads/1720194545293-img1.jpg.webp'
-                            // src={property.images[0].url} 
-                            // alt={property.title} 
-                        />
-                        <div className="p-4">
-                            <h3 className="text-xl font-semibold mb-2">
-                                carnivourius House
-                                {/* {property.title} */}
-                                </h3>
-                            <p className="text-gray-600">
-                                Kaluthara Western
-                                {/* {property.location.district}, {property.location.province} */}
-                                </p>
-                            <p className="text-gray-600">
-                                12,Colombo
-                                {/* {property.location.address} */}
-                                </p>
-                            <p className="text-gray-600">
-                                12/02/20234
-                                {/* {property.created_at} */}
-                                </p>
-                            <p className="mt-2">
-                                {/* Payment: <span className="text-yellow-600">{property.status}</span> */}
-                            </p>
-                            <p className="text-xl font-bold mt-2">RS 
-                                30000
-                                {/* {property.price_per_night} */}
-                                </p>
-                            <div className="flex justify-between mt-4">
-                                {/* <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700">Chat</button> */}
-                                <div className="flex justify-center mt-4">
-        <button className="bg-yellow-500 text-white px-70 py-2 rounded-lg hover:bg-yellow-700">Verify</button>
-    </div>
-                            </div>
-                        </div>
-                    </div>
-                {/* ))
-                } */}
+
+        <div className="container mx-auto p-4">
+            <SearchProperty />
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {properties && properties.length > 0 ? (
+                    properties.map(property => (
+                        <PendingPropertyCard key={property._id} property={property} />
+                    ))
+                ) : (
+                    <h1>No Pending Properties</h1>
+                )}
+               
             </div>
         </div>
+
     );
 };
                 
