2716a3791c7e99d5ad013bb4a016b92307bc16dc
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-07-23 23:26:39 +0530

    Navbar missing links after conflict correcte, added 'guest' user role to header and testing a new UI fro profile

--- client/src/routes/UserRoutes.jsx
+++ client/src/routes/UserRoutes.jsx
@@ -3,10 +3,11 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from '../context/auth';
 import ViewProfile from '../pages/common/ViewProfile';
 import EditProfile from '../pages/common/EditProfile';
+import Test from '../pages/common/Test'
 
 function UserRoutes() {
   const { currentUser, loading } = useAuth();
-  const allowedRoles = ['admin', 'technician', 'moderator', 'host', 'moderator' ];
+  const allowedRoles = ['admin', 'technician', 'guest', 'host', 'moderator' ];
   // console.log(currentUser);
 
   if (loading) {
@@ -17,6 +18,7 @@ function UserRoutes() {
     <Routes>
       <Route path="/ViewProfile/:id" element={currentUser && allowedRoles.includes(currentUser.role) ? <ViewProfile /> : <Navigate to="/Unauthorized" />} />
       <Route path="/EditProfile" element={currentUser && allowedRoles.includes(currentUser.role) ? <EditProfile /> : <Navigate to="/Unauthorized" />} />
+      <Route path="/Test" element={currentUser && allowedRoles.includes(currentUser.role) ? <Test /> : <Navigate to="/Unauthorized" />} />
      
     </Routes>
   );
