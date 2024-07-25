a260c351b5c452a9bc2b97a4dde756c060179413
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-07-25 13:31:37 +0530

    Update reviewRoutes.js

--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -4,6 +4,23 @@ const reviewController = require("../controllers/reviewController");
 const { authToken, requireRole } = require("../middleware/authProvider"); // Adjust as necessary
 
 // add review
-router.post( "/add", authToken, requireRole("guest"), reviewController.addReview );
+router.post(
+  "/add",
+  authToken,
+  requireRole("guest"),
+  reviewController.addReview
+);
+router.get(
+  "/hostreviews",
+  authToken,
+  requireRole("host"),
+  reviewController.getHostReviews
+);
 
+router.get(
+  "/userreviews",
+  authToken,
+  requireRole("guest"),
+  reviewController.getUserReviews
+);
 module.exports = router;
