32559262658ddeb9015c45bc2738963f1dbb1ba8
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-07-27 18:45:57 +0530

    fix issue related to add reservation

--- client/src/pages/common/PropertyDetails.jsx
+++ client/src/pages/common/PropertyDetails.jsx
@@ -1,27 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Carousel from '../../components/Carousel';
-import PropertyAmenitiesDisplay from '../host/components/PropertyAmenitiesDisplay';
-import ReservationSection from '../../components/ReservationSection';
-import PropertyHost from '../../components/PropertyHost';
-import { FaCouch, FaHome, FaClock, FaMapMarkerAlt, FaShower, FaBed, FaUserFriends, FaDoorClosed, FaBath, FaImages } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Carousel from "../../components/Carousel";
+import PropertyAmenitiesDisplay from "../host/components/PropertyAmenitiesDisplay";
+import ReservationSection from "../../components/ReservationSection";
+import PropertyHost from "../../components/PropertyHost";
+import {
+  FaCouch,
+  FaHome,
+  FaClock,
+  FaMapMarkerAlt,
+  FaShower,
+  FaBed,
+  FaUserFriends,
+  FaDoorClosed,
+  FaBath,
+  FaImages,
+} from "react-icons/fa";
 import { MdOutlineMeetingRoom } from "react-icons/md";
 import { IoBedSharp } from "react-icons/io5";
 import { GoPersonFill } from "react-icons/go";
 
 const capitalizeWords = (str) => {
-  return str.replace(/\b\w/g, char => char.toUpperCase());
+  return str.replace(/\b\w/g, (char) => char.toUpperCase());
 };
 
 const PropertyHeader = ({ title, createdAt }) => {
   return (
-    <div className='flex mb-1 border-b-4 border-blue-600 p-6 rounded-md shadow-sm bg-white'>
+    <div className="flex mb-1 border-b-4 border-blue-600 p-6 rounded-md shadow-sm bg-white">
       <h1 className="flex items-center text-4xl font-extrabold text-black-600">
         <FaHome className="mr-4" /> {title}
       </h1>
       <div className="flex items-center text-gray-600 ml-6 mt-3">
-        <FaClock className="mr-2" /> <span>Added on: {new Date(createdAt).toLocaleDateString()}</span>
+        <FaClock className="mr-2" />{" "}
+        <span>Added on: {new Date(createdAt).toLocaleDateString()}</span>
       </div>
     </div>
   );
@@ -30,7 +42,11 @@ const PropertyHeader = ({ title, createdAt }) => {
 const PropertyImages = ({ images }) => {
   return (
     <div className="bg-white rounded-lg overflow-hidden mb-4">
-      <Carousel images={images.map(img => ({ url: `${import.meta.env.VITE_API_URL}/${img.url.replace(/\\/g, '/')}` }))} />
+      <Carousel
+        images={images.map((img) => ({
+          url: `${import.meta.env.VITE_API_URL}/${img.url.replace(/\\/g, "/")}`,
+        }))}
+      />
     </div>
   );
 };
@@ -40,17 +56,24 @@ const PropertyInfo = ({ location, section }) => {
     <div className="w-full md:w-2/3 rounded-lg p-1 bg-white shadow">
       <div className="bg-white p-8 flex items-center border-b">
         <FaMapMarkerAlt className="mr-2" />
-        <p className="font-semibold text-xl">{capitalizeWords(location.address)} {capitalizeWords(location.province)}</p>
+        <p className="font-semibold text-xl">
+          {capitalizeWords(location.address)}{" "}
+          {capitalizeWords(location.province)}
+        </p>
       </div>
       <div className="bg-white p-8 flex items-center text-xl gap-4 border-b">
-        <MdOutlineMeetingRoom className='text-blue-500' /><p>Bedrooms: {section.plan.bedrooms}</p>
-        <IoBedSharp className='ml-3 text-blue-500' /><p>Beds: {section.plan.beds}</p>
-        <FaShower className='ml-3 text-blue-500' /><p>Bathrooms: {section.plan.bathrooms}</p>
-        <GoPersonFill className='ml-3 text-blue-500' /><p>Guests: {section.plan.guests}</p>
+        <MdOutlineMeetingRoom className="text-blue-500" />
+        <p>Bedrooms: {section.plan.bedrooms}</p>
+        <IoBedSharp className="ml-3 text-blue-500" />
+        <p>Beds: {section.plan.beds}</p>
+        <FaShower className="ml-3 text-blue-500" />
+        <p>Bathrooms: {section.plan.bathrooms}</p>
+        <GoPersonFill className="ml-3 text-blue-500" />
+        <p>Guests: {section.plan.guests}</p>
       </div>
       <div className="bg-white p-8 flex items-center">
         <h2 className="text-xl font-bold">Rating: </h2>
-        <p className='ml-4'>No reviews yet.</p>
+        <p className="ml-4">No reviews yet.</p>
       </div>
     </div>
   );
@@ -61,11 +84,14 @@ const PropertyInfoSections = ({ location }) => {
     <div className="w-full md:w-2/3 rounded-lg p-1 bg-white shadow">
       <div className="bg-white p-8 flex items-center border-b">
         <FaMapMarkerAlt className="mr-2" />
-        <p className="font-semibold text-xl">{capitalizeWords(location.address)} {capitalizeWords(location.province)}</p>
+        <p className="font-semibold text-xl">
+          {capitalizeWords(location.address)}{" "}
+          {capitalizeWords(location.province)}
+        </p>
       </div>
       <div className="bg-white p-8 flex items-center">
         <h2 className="text-xl font-bold">Rating: </h2>
-        <p className='ml-4'>No reviews yet.</p>
+        <p className="ml-4">No reviews yet.</p>
       </div>
     </div>
   );
@@ -82,9 +108,9 @@ const PropertyHostInfo = ({ propertyId }) => {
 
 const PropertyDescription = ({ description }) => {
   return (
-    <div className='w-full rounded-lg p-6 bg-white shadow mt-2'>
+    <div className="w-full rounded-lg p-6 bg-white shadow mt-2">
       <h2 className="text-xl font-bold mb-2">Description</h2>
-      <p className='text-lg'>{description}</p>
+      <p className="text-lg">{description}</p>
     </div>
   );
 };
@@ -94,42 +120,58 @@ const SectionDetails = ({ section }) => {
     <div className="grid grid-cols-2 md:grid-cols-4 mt-4 p-4 rounded-lg shadow-inner w-8/12">
       <div className="flex items-center mb-4">
         <FaDoorClosed className="text-2xl text-blue-600 mr-2" />
-        <p className="text-lg"><strong>Bedrooms:</strong> {section.plan.bedrooms}</p>
+        <p className="text-lg">
+          <strong>Bedrooms:</strong> {section.plan.bedrooms}
+        </p>
       </div>
       <div className="flex items-center mb-4">
         <FaBath className="text-2xl text-blue-600 mr-2" />
-        <p className="text-lg"><strong>Bathrooms:</strong> {section.plan.bathrooms}</p>
+        <p className="text-lg">
+          <strong>Bathrooms:</strong> {section.plan.bathrooms}
+        </p>
       </div>
       <div className="flex items-center mb-4">
         <FaUserFriends className="text-2xl text-blue-600 mr-2" />
-        <p className="text-lg"><strong>Guests:</strong> {section.plan.guests}</p>
+        <p className="text-lg">
+          <strong>Guests:</strong> {section.plan.guests}
+        </p>
       </div>
       <div className="flex items-center mb-4">
         <FaBed className="text-2xl text-blue-600 mr-2" />
-        <p className="text-lg"><strong>Beds:</strong> {section.plan.beds}</p>
+        <p className="text-lg">
+          <strong>Beds:</strong> {section.plan.beds}
+        </p>
       </div>
     </div>
   );
 };
 
 const PropertySection = ({ section, isExpanded, onExpand, propertyId }) => {
-  const imageUrl_0 = `${import.meta.env.VITE_API_URL}/${section.images[0].url.replace(/\\/g, '/')}`;
+  const imageUrl_0 = `${
+    import.meta.env.VITE_API_URL
+  }/${section.images[0].url.replace(/\\/g, "/")}`;
 
   return (
     <div className="mb-8 p-4 bg-white rounded-lg shadow-lg border border-gray-200">
       <div className="flex items-center cursor-pointer" onClick={onExpand}>
-        <img src={imageUrl_0} alt={section.section_name} className="w-96 h-40 object-cover rounded-lg mr-10 ml-2" />
+        <img
+          src={imageUrl_0}
+          alt={section.section_name}
+          className="w-96 h-40 object-cover rounded-lg mr-10 ml-2"
+        />
         <div className="flex flex-col justify-around w-full">
           <div className="flex justify-between items-center">
             <h3 className="text-2xl font-semibold">{section.section_name}</h3>
-            <p className="text-lg text-white bg-blue-600 rounded-md p-2"><strong>Price Per Night:</strong> Rs {section.price_per_night}</p>
+            <p className="text-lg text-white bg-blue-600 rounded-md p-2">
+              <strong>Price Per Night:</strong> Rs {section.price_per_night}
+            </p>
           </div>
           <SectionDetails section={section} />
         </div>
       </div>
       {isExpanded && (
         <>
-          <div className='flex my-6 bg-gray-100 p-4 border-round font-bold'>
+          <div className="flex my-6 bg-gray-100 p-4 border-round font-bold">
             <h1 className="flex items-center text-2xl text-black-600">
               <FaImages className="mr-4" /> More Images
             </h1>
@@ -138,7 +180,10 @@ const PropertySection = ({ section, isExpanded, onExpand, propertyId }) => {
             {section.images.slice(1).map((img, index) => (
               <img
                 key={index}
-                src={`${import.meta.env.VITE_API_URL}/${img.url.replace(/\\/g, '/')}`}
+                src={`${import.meta.env.VITE_API_URL}/${img.url.replace(
+                  /\\/g,
+                  "/"
+                )}`}
                 alt={`Section image ${index + 1}`}
                 className="w-full h-40 object-cover rounded-lg"
               />
@@ -175,7 +220,7 @@ const PropertySectionsList = ({ property }) => {
       </div>
       <PropertyDescription description={property.description} />
 
-      <div className='flex my-6 border-b-4 border-blue-600 p-6 rounded-md shadow-sm bg-white'>
+      <div className="flex my-6 border-b-4 border-blue-600 p-6 rounded-md shadow-sm bg-white">
         <h1 className="flex items-center text-3xl font-extrabold text-black-600">
           <FaCouch className="mr-4" /> Accommodation Types
         </h1>
@@ -201,12 +246,16 @@ const DetailedPropertyView = ({ property, id }) => {
       <PropertyHeader title={property.title} createdAt={property.created_at} />
       <PropertyImages images={property.images} />
       <div className="flex flex-col md:flex-row gap-4">
-        <PropertyInfo location={property.location} section={property.sections[0]} />
+        <PropertyInfo
+          location={property.location}
+          section={property.sections[0]}
+        />
         <PropertyHostInfo propertyId={property._id} />
       </div>
       <PropertyDescription description={property.description} />
       <ReservationSection
         propertyId={id}
+        sectionId={property.sections[0].section_id}
         nightlyRate={property.sections[0].price_per_night}
         initialCheckInDate="2024-07-11"
         initialCheckOutDate="2024-07-16"
@@ -224,10 +273,12 @@ function PropertyDetails() {
   useEffect(() => {
     const fetchProperty = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/properties/${id}`);
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/properties/${id}`
+        );
         setProperty(response.data);
       } catch (error) {
-        console.error('Error fetching property:', error);
+        console.error("Error fetching property:", error);
       }
     };
 
