ce4626d59927ad52977d52ff8cb112c22be2e974
Charith Lakshan <wgclakshan@gmail.com>
2024-07-29 15:59:21 +0530

    Socket.io server configuration

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -4,4 +4,21 @@ const { Server } = require('socket.io');
 
 const app = express();
 
-module.exports = { app };
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors : {
+        origin : true,
+        methods : ["GET","POST"]
+    }
+});
+
+io.on('connection',(socket)=>{
+    console.log("a user connected.",socket.id);
+
+    Socket.on("disconnect",() => {
+        console.log("user disconnected",socket.id);
+    });
+
+});
+
+module.exports = { app,io,server };
