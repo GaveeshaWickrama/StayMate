2afffab954e3c5bd30ed656ae25e6672e2a9c5e7
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-07-30 11:17:34 +0530

    commit trial

--- client/src/pages/moderator/ViewNewProperties.jsx
+++ client/src/pages/moderator/ViewNewProperties.jsx
@@ -39,7 +39,7 @@ const ViewNewProperties = () => {
 
         <div className="container mx-auto p-4">
             <SearchProperty />
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[15rem]">
+            <div className="flex justify-between items-center mb-4">
                 {properties && properties.length > 0 ? (
                     properties.map(property => (
                         <PendingPropertyCard key={property._id} property={property} />
