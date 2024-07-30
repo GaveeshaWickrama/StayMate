b0be44b179274d8bf194eef5535a5178404bebaf
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-07-30 20:13:05 +0530

    pendingproperty card styles changed

--- client/src/components/moderator/PendingPropertyCard.jsx
+++ client/src/components/moderator/PendingPropertyCard.jsx
@@ -32,13 +32,13 @@ const PendingPropertyCard = ({ property }) => {
         <div className="flex justify-center mb-2">
           <div className="font-bold text-lg">{property.title || 'No Title'}</div>
         </div>
-        <div className="text-gray-700 text-sm mb-2">
+        {/* <div className="text-gray-700 text-sm mb-2">
           <FaMapMarkerAlt className="inline-block mr-1" />
           {property.location?.address || 'No Address'}, {property.location?.province || 'No Province'}
-        </div>
+        </div> */}
         <div className="flex items-center text-gray-700 text-sm mb-2">
           <BsDiamondHalf className="mr-1 text-blue-500" /> 
-          {property.sections[0]?.section_name === 'entire_place' ? 'Entire Place' : 'Section'} {property.type || 'No Type'}
+          {property.sections[0]?.section_name === 'entire_place' ? 'Entire Place' : 'Section'} in {property.type || 'No Type'}
           <BsCalendarDate className="ml-4 mr-1 text-blue-500" /> {formattedDate}
         </div>
       </div>
@@ -49,8 +49,15 @@ const PendingPropertyCard = ({ property }) => {
         </div>
       </div>
    
-      <div className="px-4 py-2 flex justify-between items-center border-t">
-        <span className="text-xl font-bold mb-2">{property.location?.province || 'No Province'} | {property.location?.district || 'No District'}</span>
+      <div className="px-4 py-2 flex justify-center items-center border-t">
+        {/* <span className="text-xl font-bold mb-2"> */}
+          {property.location?.province || 'No Province'} 
+          {/* </span> */}
+      </div>
+      <div className="px-4 py-2 flex justify-center items-center">
+        {/* <span className="text-xl font-bold mb-2"> */}
+          {property.location?.district || 'No District'}
+          {/* </span> */}
       </div>
       <div className="px-4 py-2 flex justify-center">
         <Link
