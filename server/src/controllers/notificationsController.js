f7e200d8db9500cea7a079c713f55946b1830631
Charith Lakshan <wgclakshan@gmail.com>
2024-09-04 16:16:51 +0530

    Notification controller and the function to fetch all the unread notifications

---
+++ server/src/controllers/notificationsController.js
@@ -0,0 +1,23 @@
+const Notification = require('../models/bellNotificationModel');
+
+const getNotifications = async (req,res) => {
+
+    const loggedInUser = req.user.userId;
+
+    try {
+
+        const notifications = await Notification.find({
+            userId : loggedInUser,
+            readStatus : false
+        });
+
+        res.status(200).json(notifications);
+        
+    } catch (error) {
+        console.log("Error fetching notificatios in notification controller",error.message)
+        res.status(500).json({message: "Error fetching Notifications" })
+    }
+
+}
+
+module.exports = {getNotifications}
